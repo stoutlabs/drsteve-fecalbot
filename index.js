ce3eb31c64a38a32fc1b58b9757f80d7a79c937b
Daniel <daniel@stoutlabs.com>
2018-03-17 07:31:27 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,72 @@
+require("dotenv").config();
+
+const path = require("path");
+const fs = require("fs");
+const emoji = require("node-emoji");
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+//load up the points tracking stuff
+const Points = require("./utils/Points.js");
+const points = new Points();
+points.fetchPointsData(); // sets points.points initial data
+
+// Build Snoowrap and Snoostorm clients
+const r = new Snoowrap({
+  userAgent: "node:drsteve-fecalbot:v1.0.0",
+  clientId: process.env.FB_CLIENT_ID,
+  clientSecret: process.env.FB_CLIENT_SECRET,
+  username: process.env.FB_REDDIT_USER,
+  password: process.env.FB_REDDIT_PASS
+});
+const client = new Snoostorm(r);
+
+const streamOpts = {
+  subreddit: "testingground4bots",
+  results: 5,
+  pollTime: 25000
+};
+
+// Create a Snoostorm CommentStream with the specified options
+const comments = client.CommentStream(streamOpts);
+
+// On comment, perform whatever logic you want to do
+comments.on("comment", comment => {
+  //console.log(comment);
+  if (comment.body.toLowerCase().startsWith("!reddit")) {
+    const author = points.formatUsername(comment.author.name);
+    const commentArr = comment.body.split(" ");
+
+    let giftType = commentArr[0].replace(/!reddit/i, "");
+    if (giftType !== "") {
+      giftType = giftType.charAt(0).toUpperCase() + giftType.slice(1);
+    } else {
+      giftType = "Feces";
+    }
+
+    const poop = emoji.emojify(":poop:");
+    let message = `> ##### Here's your Reddit ${giftType}, `;
+
+    r
+      .getComment(comment.parent_id)
+      .fetch()
+      .then(parentcomment => {
+        goesTo = points.formatUsername(parentcomment.author.name);
+        points.addPoints(author, goesTo).then(newPoints => {
+          const senderData = points.fetchUserData(author);
+          const receiverData = points.fetchUserData(goesTo);
+          message += `${goesTo}! ${poop} \n\n > You have received (${
+            newPoints.received
+          }) in total, and ${author} has sent out (${newPoints.sent}) in total.`;
+
+          parentcomment.reply(message);
+          console.log(message);
+        });
+
+        return;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+});
---
+++ utils/Points.js
@@ -0,0 +1,110 @@
+const path = require("path");
+const fs = require("fs");
+
+const pointsFile = path.join(__dirname, "points.json");
+
+class Points {
+  constructor() {
+    this.points = {};
+  }
+
+  fetchPointsData() {
+    try {
+      const pointsString = fs.readFileSync(pointsFile);
+      console.log("loading initial points data.");
+      this.points = JSON.parse(pointsString);
+      return this.points;
+    } catch (err) {
+      return {};
+    }
+  }
+
+  savePointsData() {
+    try {
+      console.log("saving data to file.");
+      fs.writeFileSync(pointsFile, JSON.stringify(this.points));
+      return;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  getUserIndex(username) {
+    const userIndex = this.points.users.findIndex(user => user.username === username);
+    if (userIndex > -1) {
+      return userIndex;
+    }
+
+    return false;
+  }
+
+  fetchUserData(username) {
+    const userindex = this.points.users.findIndex(user => user.username === username);
+    if (userindex > -1) {
+      return this.points.users[userindex];
+    }
+    return false;
+  }
+
+  addPoints(usernameSender, usernameReceiver) {
+    return new Promise((resolve, reject) => {
+      let newPoints = {
+        sent: 0,
+        received: 0
+      };
+
+      //handle sender
+      let sender = this.fetchUserData(usernameSender);
+      if (sender) {
+        console.log("sent points before update:", this.points.users[this.getUserIndex(usernameSender)].sent);
+        const newSentPoints = ++this.points.users[this.getUserIndex(usernameSender)].sent;
+        newPoints.sent = newSentPoints;
+        console.log("newSentPoints:", newSentPoints);
+      } else {
+        sender = {
+          username: usernameSender,
+          sent: 1,
+          received: 0
+        };
+        this.points.users.push(sender);
+        newPoints.sent = 1;
+      }
+
+      //handle receiver
+      let receiver = this.fetchUserData(usernameReceiver);
+      if (receiver) {
+        console.log("receive points before update:", this.points.users[this.getUserIndex(usernameReceiver)].received);
+        const newRecPoints = ++this.points.users[this.getUserIndex(usernameReceiver)].received;
+        console.log("newRecPoints:", newRecPoints);
+        newPoints.received = newRecPoints;
+      } else if (usernameSender !== usernameReceiver) {
+        receiver = {
+          username: usernameReceiver,
+          sent: 0,
+          received: 1
+        };
+        this.points.users.push(receiver);
+        newPoints.received = 1;
+      }
+
+      //fs.writeFileSync(pointsFile, JSON.stringify(this.points));
+      this.savePointsData();
+
+      resolve(newPoints);
+    });
+  }
+
+  formatUsername(rawName) {
+    if (typeof rawName === "string") {
+      let formattedName = rawName;
+      if (!formattedName.startsWith("u/")) {
+        formattedName = "u/" + formattedName;
+      }
+      return formattedName;
+    }
+
+    return false;
+  }
+}
+
+module.exports = Points;
