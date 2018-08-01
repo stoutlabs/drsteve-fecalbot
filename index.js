d4ac85614f32d496ef98a35423fe7329189a73bd
Daniel <daniel@stoutlabs.com>
2018-08-01 01:13:10 -0400

    Correction to output

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ comments.on("comment", comment => {
     let giftType = commentArr[0].replace(/!reddit/i, "");
     if (giftType !== "") {
       giftType = giftType.charAt(0).toUpperCase() + giftType.slice(1);
-      giftType = giftType.replace(/_|-/gi, " ");
+      giftType = giftType.replace(/(\\_)|(\\-)/gi, " ");
     } else {
       giftType = "Feces";
     }
@@ -65,7 +65,7 @@ comments.on("comment", comment => {
             }) in total, and ${author} has sent out (${newPoints.sent}) in total.`;
 
             parentcomment.reply(message);
-            console.log(message);
+            //console.log(message);
           })
           .catch(e => {
             console.log(e);
