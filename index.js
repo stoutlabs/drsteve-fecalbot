833f10ce39216517b8e133300aadd86b00128803
Daniel <daniel@stoutlabs.com>
2018-07-31 23:18:58 -0400

    Add functionality to allow multi word 'awards'

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ comments.on("comment", comment => {
     let giftType = commentArr[0].replace(/!reddit/i, "");
     if (giftType !== "") {
       giftType = giftType.charAt(0).toUpperCase() + giftType.slice(1);
+      giftType = giftType.replace(/_|-/gi, " ");
     } else {
       giftType = "Feces";
     }
